1f43bf6f7c7e3d1890b7a12a7b08958442d861d3
Filip N. Kotlarz <fkotlarz@gmail.com>
2014-08-27 21:41:22 +0200

    Create Greeting.java

---
+++ Greeting.java
@@ -0,0 +1,34 @@
+public class Greeting {
+	
+	String _hilsen = null;
+	String _navn = null;
+	
+	
+	public void SetHilsen(String newValue)
+	{		
+		_hilsen = newValue;
+		
+	}	
+	public String GetHilsen(int option)
+	{
+		if (option == 0) {return _hilsen; }
+		else if (option == 1)
+		{
+			String uttekst = "Din hilsen er: " + "\n" + _hilsen;
+			return uttekst;
+		}
+		else {
+			return null;
+		}
+	}	
+	public void SetNavn(String newValue)
+	{
+		_navn = newValue;
+	}
+	public String GetNavn()
+	{
+		String hilsen = "Hallo " + _navn;
+		return hilsen;
+	}
+		
+}
